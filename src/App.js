110872e06ddb3be0365c023417c637e0f31e630e
saragam <git@saragam.com.np>
2023-05-11 09:03:11 +0545

    helmet removed

--- src/App.js
+++ src/App.js
@@ -5,13 +5,9 @@ import { v4 as uuid } from "uuid";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Dashboard from "./components/Dashboard";
-import { Helmet } from "react-helmet";
 
 const App = () => {
-  const pageTitle = "React-todo-list App";
-  const pageDescription =
-    "This is a simple todo app built using ReactJS. It allows users to create new todos, mark them as complete, and delete them. All todos are saved to local storage and accessed from there every time the app is started, and any changes made to the todos are also saved to local storage o that they are accessible even after the user closes the app.";
-  const pageImage = "todo.jpg";
+ 
 
   const [todos, setTodos] = useState([]);
   const [newItem, setNewItem] = useState({
@@ -71,14 +67,6 @@ const App = () => {
 
   return (
     <>
-      <Helmet>
-        <title>{pageTitle}</title>
-        <meta name="description" content={pageDescription} />
-        <meta property="og:title" content={pageTitle} />
-        <meta property="og:description" content={pageDescription} />
-        <meta property="og:image" content={require(`./images/${pageImage}`)} />
-      </Helmet>
-
       <div className="app">
         <Header />
         <div className="container">
