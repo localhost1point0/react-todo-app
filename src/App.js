7fc4cbe0186c37ae4f1d6f30fd06d20b22aba801
saragam <git@saragam.com.np>
2023-05-11 11:22:20 +0545

    helmet-async added

--- src/App.js
+++ src/App.js
@@ -5,10 +5,9 @@ import { v4 as uuid } from "uuid";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Dashboard from "./components/Dashboard";
+import { HelmetProvider, Helmet } from "react-helmet-async";
 
 const App = () => {
- 
-
   const [todos, setTodos] = useState([]);
   const [newItem, setNewItem] = useState({
     id: "",
@@ -65,8 +64,23 @@ const App = () => {
     setAndSave(filteredTodo);
   };
 
+  const article = {
+    title: "A Step-by-Step Guide",
+    description:
+      "A dummies guide to getting up and running with selenium-webdriver",
+    image: "https://miro.medium.com/max/812/1*1xhuVp8f2WFUGUByHS8VTg.png",
+  };
+
   return (
-    <>
+    <HelmetProvider>
+      <Helmet>
+        <title>{article.title}</title>
+        <meta name="description" content={article.description} />
+        <meta property="og:title" content={article.title} />
+        <meta property="og:description" content={article.description} />
+        <meta property="og:image" content={article.image} />
+      </Helmet>
+
       <div className="app">
         <Header />
         <div className="container">
@@ -88,7 +102,7 @@ const App = () => {
         </div>
         <Footer />
       </div>
-    </>
+    </HelmetProvider>
   );
 };
 
